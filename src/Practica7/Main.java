40867012ab710a789899cb6725295888d8d154b7
tdwda <tdwda@LAPTOP-CLED55LT>
2021-06-19 21:09:36 -0500

    v 1.2 - RMI server funcional

--- src/Practica7/Main.java
+++ src/Practica7/Main.java
@@ -7,6 +7,8 @@ package Practica7;
 
 import java.io.IOException;
 import java.net.MulticastSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.UUID;
@@ -17,14 +19,27 @@ import java.util.UUID;
  */
 public class Main {
     
-    public static void main(String main[]) throws IOException{
+    public static int puertosDisponibles(int lim_menor, int lim_superior){
+        for (int port = lim_menor; port < lim_superior; port++) {
+            try{
+                ServerSocket s = new ServerSocket(port);
+                s.close();
+                return port;
+            }catch(Exception e){}
+        }
+        return 0;
+    }
+    
+    public static void main(String main[]) throws IOException, InterruptedException{
         
         Scanner sc = new Scanner(System.in);
-        int rmi_envio = 8000;
-        int flujo_envio = 7778;
+        int rmi_envio = puertosDisponibles(1999, 4000);
+        int flujo_envio = puertosDisponibles(7778, 1000);
         String uniqueID = UUID.randomUUID().toString();
         
         ArrayList <Nodo> nodos_encontrados = new ArrayList();
+        
+        ArrayList <Lista_elem> allFiles = new ArrayList();
         
         MulticastSocket socket;
         socket = new MulticastSocket(7777);
@@ -32,19 +47,45 @@ public class Main {
         socket.setTimeToLive(255);
         
         //Inicialización de hilos para anuncio de puertos y descubrimiento de nodos
-        System.out.println("ID de sesio: " + uniqueID);
+        System.out.println("ID de sesion: " + uniqueID);
+        System.out.println("*************************************************");
         new ServidorMulticast(socket, rmi_envio, flujo_envio, uniqueID, "Puertos").start();
         new ClienteNodos(socket, nodos_encontrados, rmi_envio, flujo_envio, uniqueID).start();
         
+        //System.out.println("Escriba el directorio donde se realizaran las peticiones de busqueda: ");
+        //String Dir_Busqueda = sc.nextLine();
+        System.out.println("Inicializando servidor RMI...");
         
-        System.out.println("*************************************************");
+        new Server_RMI(rmi_envio).start();
+        
+        Thread.sleep(1000);
+        
+        //"C:\\Users\\tdwda\\OneDrive\\Documentos\\NetBeansProjects\\Practica7\\Archivos"
+        //Archivos
+        
         System.out.println("Escriba el nombre del archivo que desea buscar entre los servidores: ");
-        System.out.println("*************************************************");
-        
         String archivo = sc.nextLine();
         
+        ArrayList <ListaResponse> respuestas = new ArrayList();
         
+        for (Nodo encontrado : nodos_encontrados) {
+            ArrayList <Lista_elem> respuesta = new ArrayList();
+            new Client_RMI(archivo, Integer.parseInt(encontrado.getPuerto_RMI()), respuesta).start();
+            respuestas.add(new ListaResponse(respuesta));
+        }
         
+        Thread.sleep(1000);
+        for (ListaResponse respuesta : respuestas) {
+            allFiles.addAll(respuesta.getArchivos());
+        }
+        
+        System.out.println("Se encontraron " + allFiles.size() + " archivos que coinciden con ese nombre");
+        
+        for (int i = 0; i < allFiles.size(); i++) {
+            System.out.println("Archivo " + i + " :");
+            System.out.println("\t" + allFiles.get(i).getArchivo());
+            System.out.println("\t" + allFiles.get(i).getHash());
+        }
         
     }
 }
